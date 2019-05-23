ca0eded4c49580f629f38747d58dd9717706f319
Heino <heinoolen@gmail.com>
2019-05-23 18:04:59 +0300

    Loo kangelaste listi ja kangelaste nimed

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Kangelased
 {
     class Program
     {
+        static List<Kangelane> kangelased = new List<Kangelane>();
+        static void LaeKangelased(string Failinimi)
+        {
+            string[] failiandmed = File.ReadAllLines(Failinimi);
+            foreach (string rida in failiandmed)
+            {
+                string[] name_and_location = rida.Split('/');
+                for (int i = 0; i < name_and_location.Length; i++)
+                {
+                    name_and_location[i] = name_and_location[i].Trim();
+                }
+                if (name_and_location[0].EndsWith("*"))
+                {
+                    kangelased.Add(new SuperKangelane(name_and_location[0], name_and_location[1]));
+                }
+                else
+                {
+                    kangelased.Add(new Kangelane(name_and_location[0], name_and_location[1]));
+                }
+            }
+            
+        }
         static void Main(string[] args)
         {
              
